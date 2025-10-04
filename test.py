67a10141739f81ba2347687a30c34ee4f45c1d43
LimingShuLi <li53114672@outlook.com>
2025-10-05 01:17:09 +0800

    version3.0

--- test.py
+++ test.py
@@ -4,3 +4,17 @@ greet = "你好！"
 print(greet_english + "张三")
 print(greet + "李四")
 print("She said:\"Let's go!\"")
+
+import math
+a = 1
+b = 3
+c = 2
+alpha=b**2 - 4*a*c
+print((-b + math.sqrt(alpha) )/2*a)
+print((-b - math.sqrt(alpha) )/2*a)
+# 这是一行没用的注释
+"""这也是一行没用的注释
+不对，这有多行"""
+print((-b + (alpha)**(1/2) )/2*a)
+
+
