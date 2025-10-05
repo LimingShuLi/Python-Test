4e252063ab53bcfdc63f2c6d8497cfedac83afa5
LimingShuLi <li53114672@outlook.com>
2025-10-05 17:48:05 +0800

    version6.0

---
+++ test3.py
@@ -0,0 +1,17 @@
+gender = input("你的性别是：")
+if gender == "男":
+    height = input("你的身高是（单位：m）：")
+    if float(height)>=1.75:
+       print("恭喜，你可以报名参加国旗护卫队！")
+       print("ദ്ദി˶˃ ᵕ ˂ )✧")
+    else :
+       print("可惜，你的身高不够报名国旗护卫队")
+       print("(´。＿。｀)")
+else:
+    height = input("你的身高是（单位：m）：")
+    if float(height) >= 1.65:
+        print("恭喜，你可以报名参加国旗护卫队！")
+        print("ദ്ദി˶˃ ᵕ ˂ )✧")
+    else:
+        print("可惜，你的身高不够报名国旗护卫队")
+        print("(´。＿。｀)")
