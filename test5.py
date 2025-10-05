7609e63e7a8a0bd0c506289826c84bb87e747493
LimingShuLi <li53114672@outlook.com>
2025-10-05 18:34:02 +0800

    version8.0

---
+++ test5.py
@@ -0,0 +1,15 @@
+
+userage=int(input("请输入您的年龄："))
+if userage>=18 and userage<70 :
+    print("您有资格学车")
+else :
+    print("您没有学车的资格")
+
+userage=int(input("请输入您的年龄："))
+if userage>=18:
+    if userage<=70:
+        print("您有资格学车")
+    else:
+        print("您没有学车的资格")
+else :
+    print("您没有学车的资格")
