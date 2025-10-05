a11b333531a1e8bd366c5f75c16641499faced20
LimingShuLi <li53114672@outlook.com>
2025-10-05 12:18:53 +0800

    version4.0

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ print(greet_english + "张三")
 print(greet + "李四")
 print("She said:\"Let's go!\"")
 
-import math
+import math# 引入函数库
 a = 1
 b = 3
 c = 2
@@ -17,4 +17,16 @@ print((-b - math.sqrt(alpha) )/2*a)
 不对，这有多行"""
 print((-b + (alpha)**(1/2) )/2*a)
 
-
+print(len(" l!\n"))# len函数可以返还字符串的长度
+print("6 l!\n"[0])# 字符索引从零开始
+print("6 l!\n"[len(" l!\n") -1])
+# type函数可以返还该数据的类型，比如str即为字符串
+print(type("Hello"))
+print(type(2))
+print(type(2.0))
+b1=True
+b2=False
+n=None
+print(type(b1))# 布尔类型
+print(type(b2))# 布尔类型
+print(type(n))# 空值类型
